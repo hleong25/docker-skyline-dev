70f53e95feb89dd093149de09246851c220cc8dc
henry leong <hleong25@gmail.com>
2016-12-19 16:18:35 -0800

    update vscode 1.8.1-1482158209

--- create-image/scripts/motd.sh
+++ create-image/scripts/motd.sh
@@ -11,7 +11,7 @@ cat<<EOF
              |___/
 
 Skyline development environment (ubuntu:xenial)
-Created on 2016-12-15
+Created on 2016-12-19
 
 Read more about the eCDM software onboarding: https://dpadsw.lss.emc.com/display/eCDM/Software+Onboarding
 
@@ -24,7 +24,7 @@ Installed dev apps/tools
   + bower
   + gulp
 + mongodb 3.2.1
-+ visual studio code 1.8.0-1481651903
++ visual studio code 1.8.1-1482158209
 + golang 1.7.3
 
 Useful scripts in ($INSTALL_HOME/bin)
